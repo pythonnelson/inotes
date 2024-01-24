a4fec8f0f14db13c8b326d17a42c2fdcad48ae3e
Isaac NSB Kargbo <isaacnsbkargbo20@gmail.com>
2024-01-24 13:24:38 -0800

    New note and delete button functionality

--- src/renderer/src/store/index.ts
+++ src/renderer/src/store/index.ts
@@ -20,3 +20,34 @@ export const selectedNoteAtom = atom((get) => {
     content: `You selected note ${selectedNoteIndex}`
   }
 })
+
+// CREATE NOTE
+export const createEmptyNoteAtom = atom(null, (get, set) => {
+  const notes = get(notesAtom)
+
+  const title = `Note ${notes.length + 1}`
+
+  const newNote: NoteInfo = {
+    title,
+    lastEditTime: Date.now()
+  }
+
+  set(notesAtom, [newNote, ...notes.filter((note) => note.title !== newNote.title)])
+
+  set(selectedNoteIndexAtom, 0)
+})
+
+// DELETE NOTE
+export const deleteNoteAtom = atom(null, (get, set) => {
+  const notes = get(notesAtom)
+  const selectedNote = get(selectedNoteAtom)
+
+  if (!selectedNote) return
+
+  set(
+    notesAtom,
+    notes.filter((note) => note.title !== selectedNote.title)
+  )
+
+  set(selectedNoteIndexAtom, null)
+})
