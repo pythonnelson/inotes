4e5c946b2fe246f618346d6ee768afd10d421464
Isaac NSB Kargbo <isaacnsbkargbo20@gmail.com>
2024-01-24 16:54:28 -0800

    Create new note

--- src/main/lib/index.ts
+++ src/main/lib/index.ts
@@ -1,9 +1,11 @@
 /* eslint-disable prettier/prettier */
 import { appDirectoryName, fileEncoding } from '@shared/constants'
 import { NoteInfo } from '@shared/models'
-import { GetNotes, ReadNote, WriteNote } from '@shared/types'
+import { CreateNote, GetNotes, ReadNote, WriteNote } from '@shared/types'
+import { dialog } from 'electron'
 import { ensureDir, readFile, readdir, stat, writeFile } from 'fs-extra'
 import { homedir } from 'os'
+import path from 'path'
 
 export const getRootDir = () => {
   return `${homedir()}/${appDirectoryName}`
@@ -43,4 +45,43 @@ export const writeNote: WriteNote = async (filename, content) => {
 
   console.info(`Writing note ${filename}`)
   return writeFile(`${rootDir}/${filename}.md`, content, { encoding: fileEncoding })
+}
+
+// CREATE NOTE
+export const createNote: CreateNote = async () => {
+  const rootDir = getRootDir()
+
+  await ensureDir(rootDir)
+
+  const { filePath, canceled } = await dialog.showSaveDialog({
+    title: 'New note',
+    defaultPath: `${rootDir}/Untitled.md`,
+    buttonLabel: 'Create',
+    properties: ['showOverwriteConfirmation'],
+    showsTagField: false,
+    filters: [{ name: 'Markdown', extensions: ['md'] }]
+  })
+
+  if (canceled || !filePath) {
+    console.info('Note creation canceled')
+    return false
+  }
+
+  const { name: filename, dir: parentDir } = path.parse(filePath)
+
+  if (parentDir !== rootDir) {
+    await dialog.showMessageBox({
+      type: 'error',
+      title: 'Creation failed',
+      message: `All notes must be saved under ${rootDir}.
+      Avoid using other directories!`
+    })
+
+    return false
+  }
+
+  console.info(`Creating note to: ${filePath}`)
+  await writeFile(filePath, '')
+
+  return filename
 }
